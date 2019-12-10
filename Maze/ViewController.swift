c3bf8a2c2365875784b3071792d893c87d621cb6
Ethan Humphrey <ethantheinnovator@gmail.com>
2019-12-10 08:13:02 -0500

    Initial Commit

---
+++ Maze/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Maze
+//
+//  Created by Ethan Humphrey on 12/10/19.
+//  Copyright © 2019 Ethan Humphrey. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
